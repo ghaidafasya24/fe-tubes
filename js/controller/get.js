6df04619f5dc3674271e1ced694cbaa3d6def618
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-07-12 15:26:01 +0700

    pls insertt

--- js/controller/get.js
+++ js/controller/get.js
@@ -15,8 +15,8 @@ function isiRow(value) {
             .replace("#KATEGORI#", value.kategori.kategori)
             .replace("#BAHANBAKU#", value.bahan_baku.bahan_baku)
             .replace("#JUMLAH#", value.bahan_baku.jumlah)
-            .replace("#IDEDIT#", value._id)
-            .replace("#IDHAPUS#", value._id)
+            // .replace("#IDEDIT#", value._id)
+            // .replace("#IDHAPUS#", value._id)
             // .replace("#IDDETAIL#", value._id)
             // .replace("#WARNA#", getRandomColor())
             // .replace(/#WARNALOGO#/g, getRandomColorName());
