c532e39b1a237946ad2db17bd324705b828c33eb
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-07-12 16:21:05 +0700

    bingung

---
+++ js/controller/delete.js
@@ -0,0 +1,23 @@
+function confirmDelete(IDHAPUS) {
+    if (confirm("Apakah ingin menghapus data ID " + IDHAPUS + "?")) {
+        deleteData(IDHAPUS);
+    }
+}
+
+function deleteData(IDHAPUS) {
+    var presensiId = IDHAPUS;
+    var target_url = "https://bp-tubes-c48fa88ca6a5.herokuapp.com/delete/" + restoranId;
+
+    var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            alert(result.message);
+            location.reload();
+        })
+        .catch(error => console.log('Error:', error));
+}
