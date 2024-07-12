2d97fc0b51bb24509a3bd55ca9c54572da78a961
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-07-12 16:51:49 +0700

    bismillah

--- js/controller/edit.js
+++ js/controller/edit.js
@@ -1,27 +1,27 @@
-export function isiData(results) {  
-    const inputMapping = [
-        { id: 'nama', path: 'nama' },
-        { id: 'harga', path: 'harga' },
-        { id: 'deskripsi', path: 'deskripsi' },
-        { id: 'kategori', path: 'kategori.kategori' },
-        { id: 'bahan_baku', path: 'bahan_baku.bahan_baku' },
-        { id: 'jumlah', path: 'bahan_baku.jumlah' }
-    ];
-  
-    inputMapping.forEach(({ id, path, index, property }) => {
-      const inputElement = document.getElementById(id);
-      const value = getNestedValue(results, path, index, property);
-      inputElement.value = value;
-    });
+export function isiData(results) {
+  const inputMapping = [
+      { id: 'nama', path: 'nama' },
+      { id: 'harga', path: 'harga' },
+      { id: 'deskripsi', path: 'deskripsi' },
+      { id: 'kategori', path: 'kategori.kategori' },
+      { id: 'bahan_baku', path: 'bahan_baku.bahan_baku' },
+      { id: 'jumlah', path: 'bahan_baku.jumlah' }
+  ];
+
+  inputMapping.forEach(({ id, path, index, property }) => {
+    const inputElement = document.getElementById(id);
+    const value = getNestedValue(results, path, index, property);
+    inputElement.value = value;
+  });
 }
-  
+
 function getNestedValue(obj, path, index, property) {
-    const value = path.split('.').reduce((value, key) => (value && value[key]) ? value[key] : '', obj);
-    // console.log(`Value at path ${path}:`, value);
-  
-    if (Array.isArray(value) && value.length > index && value[index].hasOwnProperty(property)) {
-      return value[index][property];
-    }
-  
-    return value;
+  const value = path.split('.').reduce((value, key) => (value && value[key]) ? value[key] : '', obj);
+  // console.log(`Value at path ${path}:`, value);
+
+  if (Array.isArray(value) && value.length > index && value[index].hasOwnProperty(property)) {
+    return value[index][property];
+  }
+
+  return value;
 }
